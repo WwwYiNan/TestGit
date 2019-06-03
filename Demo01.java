387a1990bcd1f2893c8100febfa231966a4634b3
wang <3315554772@qq.com>
2019-06-03 09:04:10 +0800

    十四周周一

--- Demo01.java
+++ Demo01.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Demo01 {
 
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream("D:\\a.txt");
+      /*  FileInputStream fis = new FileInputStream("D:\\a.txt");
         int b=0;
         while (true) {
             b=fis.read();
@@ -17,12 +17,12 @@ public class Demo01 {
             }
             System.out.println((char)b);
         }
-
-        FileOutputStream out = new FileOutputStream("example.txt");
-        String str ="王逸楠";
+*/
+        FileOutputStream out = new FileOutputStream("example.txt",true);
+        String str ="hahahahahahahhhhhh";
         byte[] c = str.getBytes();
         for (int i=0;i<c.length;i++){
-
+            out.write(c[i]);
         }
     }
 }
